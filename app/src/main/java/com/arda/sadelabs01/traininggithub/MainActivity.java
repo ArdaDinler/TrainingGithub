9b1fd1733e9a675427b78a06ca0b07568bfe23c4
Arda Dinler <ardadinler100@gmail.com>
2017-05-05 10:37:49 +0300

    Initial commit

---
+++ app/src/main/java/com/arda/sadelabs01/traininggithub/MainActivity.java
@@ -0,0 +1,13 @@
+package com.arda.sadelabs01.traininggithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
