bd064ef1f1178ec2a72483413c863b8ce6383765
Arda Dinler <ardadinler100@gmail.com>
2017-05-05 15:22:01 +0300

    Change User Interface MEssage

--- app/src/main/java/com/arda/sadelabs01/traininggithub/MainActivity.java
+++ app/src/main/java/com/arda/sadelabs01/traininggithub/MainActivity.java
@@ -2,6 +2,8 @@ package com.arda.sadelabs01.traininggithub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText textView = (EditText) findViewById(R.id.TestTextView);
+        textView.setText("Github Hello World", TextView.BufferType.EDITABLE);
+
     }
 }
